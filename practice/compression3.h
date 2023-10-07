aeac6f19bf7989efeaf0c32ca02191132ddfb4c4
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2023-10-07 07:25:40 +0300

    put code in a namespace

--- practice/compression3.h
+++ practice/compression3.h
@@ -8,166 +8,224 @@
 
 #include <vector>
 
-using namespace std;
+namespace qfs {
 
-typedef unsigned int uint;
-typedef vector<unsigned char> bytes;
+	using namespace std;
 
-//copy length bytes from src at srcPos to dst at dstPos and increment srcPos and dstPos
-//copying one byte at a time is REQUIRED in some cases, otherwise decompression will not work
-void copyBytes(bytes& src, uint& srcPos, bytes& dst, uint& dstPos, uint length) {
-	for(uint i = 0; i < length; i++) {
-		dst[dstPos++] = src[srcPos++];
+	typedef unsigned int uint;
+	typedef vector<unsigned char> bytes;
+
+	//copy length bytes from src at srcPos to dst at dstPos and increment srcPos and dstPos
+	//copying one byte at a time is REQUIRED in some cases, otherwise decompression will not work
+	void copyBytes(bytes& src, uint& srcPos, bytes& dst, uint& dstPos, uint length) {
+		for(uint i = 0; i < length; i++) {
+			dst[dstPos++] = src[srcPos++];
+		}
 	}
-}
 
-template<typename T1, typename T2>
-T1 getMin(T1 a, T2 b) {
-    return a <= b ? a : b;
-}
+	template<typename T1, typename T2>
+	T1 getMin(T1 a, T2 b) {
+		return a <= b ? a : b;
+	}
 
-struct Match {
-	uint location;
-	uint length;
-	uint offset;
-};
+	struct Match {
+		uint location;
+		uint length;
+		uint offset;
+	};
 
-//the hash chain is similar to a hash map, the most recent values are stored in head, and past values are stored in prev
-//it requires very little memory allocation/deallocation, and the allocation is only done once, which makes it very efficient
-//head is a hash map where the key is a hash created from a number of bytes in src
-//and the value is the last position where the bytes could be found
-//prev is a an array where the index is the position masked by the length of the sliding window
-//and the value is the previous position that resolves to the same hash
-class HashChain {
-	private:
-		bytes& src;
-		vector<uint> head = vector<uint>(0x10000, 0xFFFFFFFF);
-		vector<uint> prev = vector<uint>(0x20000);
-		uint lastPos = 0;
-		uint lastHash = 0;
-		
-		//a rolling hash calculated from the last hash value
-		//in this version this has no impact on performance, so a regular hashing function could be used instead
-		//if the hash is more complicated then a rolling hash might help reduce the amount of computation done
-		//a good hashing function is needed to get good performance
-		uint updateHash(uint pos) {
-			lastHash = ((lastHash << 8) | src[pos + 1]) & 0xFFFF;
-			return lastHash;
-		}
-		
-		//get the previous position that resolves to the same hash, or 0xFFFFFFFF if the position is invalid
-		uint getPrevPos(uint prevPos, uint pos) {
-			uint nextPrevPos = prev[prevPos & 0x1FFFF];
-			if(pos - nextPrevPos > 131072 || nextPrevPos >= prevPos) {
-				return 0xFFFFFFFF;
+	//the hash chain is similar to a hash map, the most recent values are stored in head, and past values are stored in prev
+	//it requires very little memory allocation/deallocation, and the allocation is only done once, which makes it very efficient
+	//head is a hash map where the key is a hash created from a number of bytes in src
+	//and the value is the last position where the bytes could be found
+	//prev is a an array where the index is the position masked by the length of the sliding window
+	//and the value is the previous position that resolves to the same hash
+	class HashChain {
+		private:
+			bytes& src;
+			vector<uint> head = vector<uint>(0x10000, 0xFFFFFFFF);
+			vector<uint> prev = vector<uint>(0x20000);
+			uint lastPos = 0;
+			uint lastHash = 0;
+			
+			//a rolling hash calculated from the last hash value
+			//in this version this has no impact on performance, so a regular hashing function could be used instead
+			//if the hash is more complicated then a rolling hash might help reduce the amount of computation done
+			//a good hashing function is needed to get good performance
+			uint updateHash(uint pos) {
+				lastHash = ((lastHash << 8) | src[pos]) & 0xFFFF;
+				return lastHash;
 			}
 			
-			return nextPrevPos;
-		}
-		
-		//find out how long the match is, and check if the length and offset are valid
-		//depending on the hashing function you might also need to check the first 2-3 bytes here
-		Match getMatch(uint prevPos, uint pos) {
-			uint length = 2;
-			uint maxLen = getMin(src.size() - pos, 1028);
-			
-			while(length < maxLen && src[prevPos + length] == src[pos + length]) {
-				length++;
-			}
-			
-			uint offset = pos - prevPos;
-			
-			if((length >= 3 && offset <= 1024) || (offset <= 16384 && length >= 4) || length >= 5) {
-				return Match{pos, length, offset};
-			} else {
-				return Match{0, 0, 0};
-			}
-		}
-		
-	public:
-		//init hash
-		HashChain(bytes& buffer): src(buffer) {
-			lastHash = src[0];
-		}
-		
-		//add all bytes from lastPos to pos
-		void addTo(uint pos) {
-			for(lastPos; lastPos <= pos; lastPos++) {
-				uint hashVal = updateHash(lastPos);
-				prev[lastPos & 0x1FFFF] = head[hashVal];
-				head[hashVal] = lastPos;
-			}
-		}
-		
-		//check past values and find the longest matching pattern
-		Match getLongestMatch(uint pos) {
-			Match longestMatch = Match{0, 0, 0};
-			
-			//it's important to limit the number of loops here for the sake of speed
-			uint prevPos = getPrevPos(pos, pos);
-			uint n_chains = 0;
-			
-			while(prevPos != 0xFFFFFFFF && n_chains < 100) {
-				Match match = getMatch(prevPos, pos);
-				
-				if(match.length > longestMatch.length) {
-					longestMatch = match;
-					
-					if(longestMatch.length >= 100) {
-						break;
-					}
+			//get the previous position that resolves to the same hash, or 0xFFFFFFFF if the position is invalid
+			uint getPrevPos(uint prevPos, uint pos) {
+				uint nextPrevPos = prev[prevPos & 0x1FFFF];
+				if(pos - nextPrevPos > 131072 || nextPrevPos >= prevPos) {
+					return 0xFFFFFFFF;
 				}
 				
-				prevPos = getPrevPos(prevPos, pos);
-				n_chains++;
+				return nextPrevPos;
 			}
 			
-			return longestMatch;
-		}
-};
+			//find out how long the match is, and check if the length and offset are valid
+			//depending on the hashing function you might also need to check the first 2-3 bytes here
+			Match getMatch(uint prevPos, uint pos) {
+				uint length = 2;
+				uint maxLen = getMin(src.size() - pos, 1028);
+				
+				while(length < maxLen && src[prevPos + length] == src[pos + length]) {
+					length++;
+				}
+				
+				uint offset = pos - prevPos;
+				
+				if((length >= 3 && offset <= 1024) || (offset <= 16384 && length >= 4) || length >= 5) {
+					return Match{pos, length, offset};
+				} else {
+					return Match{0, 0, 0};
+				}
+			}
+			
+		public:
+			//init hash
+			HashChain(bytes& buffer): src(buffer) {
+				updateHash(0);
+			}
+			
+			//add all bytes from lastPos to pos
+			void addTo(uint pos) {
+				for(lastPos; lastPos <= pos; lastPos++) {
+					uint hashVal = updateHash(lastPos + 1);
+					prev[lastPos & 0x1FFFF] = head[hashVal];
+					head[hashVal] = lastPos;
+				}
+			}
+			
+			//check past values and find the longest matching pattern
+			Match getLongestMatch(uint pos) {
+				Match longestMatch = Match{0, 0, 0};
+				
+				//it's important to limit the number of loops here for the sake of speed
+				uint prevPos = getPrevPos(pos, pos);
+				uint n_chains = 0;
+				
+				while(prevPos != 0xFFFFFFFF && n_chains < 100) {
+					Match match = getMatch(prevPos, pos);
+					
+					if(match.length > longestMatch.length) {
+						longestMatch = match;
+						
+						if(longestMatch.length >= 100) {
+							break;
+						}
+					}
+					
+					prevPos = getPrevPos(prevPos, pos);
+					n_chains++;
+				}
+				
+				return longestMatch;
+			}
+	};
 
-//compresses src, returns an empty vector if compression fails
-//it will fail if the compressed output >= decompressed input since it's better to store these assets uncompressed
-//this typically happens with very small assets
-bytes compress(bytes& src) {
-	//finding patterns
-	HashChain chain = HashChain(src);
-	vector<Match> matches = vector<Match>();
-	
-	uint i = 0;
-	while(i <= src.size() - 3) {
-		chain.addTo(i);
-		Match match = chain.getLongestMatch(i);
+	//compresses src, returns an empty vector if compression fails
+	//it will fail if the compressed output >= decompressed input since it's better to store these assets uncompressed
+	//this typically happens with very small assets
+	bytes compress(bytes& src) {
+		//finding patterns
+		HashChain chain = HashChain(src);
+		vector<Match> matches = vector<Match>();
 		
-		if(match.length >= 3) {
-			matches.push_back(match);
-			i += match.length;
-		} else {
-			i++;
+		uint i = 0;
+		while(i <= src.size() - 3) {
+			chain.addTo(i);
+			Match match = chain.getLongestMatch(i);
+			
+			if(match.length >= 3) {
+				matches.push_back(match);
+				i += match.length;
+			} else {
+				i++;
+			}
 		}
-	}
-	
-	//compress src
-	//compressed output has to smaller than the decompressed output, otherwise it's not useful
-	//maximum possible size of the compressed entry is 0xFFFFFF + 1 due to the fact that the compressed size in the header is only 3 bytes long
-	bytes dst = bytes(getMin(src.size() - 1, 16777216));
-	
-	uint srcPos = 0;
-	uint dstPos = 9;
-	
-	//plain = number of bytes to copy from src as is with no compression
-	//nCopy = number of bytes to compress
-	//offset = offset from current location in decompressed input to where the pattern could be found
-	
-	for(uint i = 0; i < matches.size(); i++) {
-		//copy bytes from src to dst until the location of the match is reached
-		uint plain = matches[i].location - srcPos;
+		
+		//compress src
+		//compressed output has to smaller than the decompressed output, otherwise it's not useful
+		//maximum possible size of the compressed entry is 0xFFFFFF + 1 due to the fact that the compressed size in the header is only 3 bytes long
+		bytes dst = bytes(getMin(src.size() - 1, 16777216));
+		
+		uint srcPos = 0;
+		uint dstPos = 9;
+		
+		//plain = number of bytes to copy from src as is with no compression
+		//nCopy = number of bytes to compress
+		//offset = offset from current location in decompressed input to where the pattern could be found
+		
+		for(uint i = 0; i < matches.size(); i++) {
+			//copy bytes from src to dst until the location of the match is reached
+			uint plain = matches[i].location - srcPos;
+			while(plain > 3) {
+				//max possible value is 112
+				//this can only be a multiple of 4, due to the 2-bit right shift in the control character
+				plain = getMin(plain - plain % 4, 112);
+				
+				if(dstPos + plain + 1 > dst.size()) {
+					return bytes();
+				}
+				
+				// 111ppppp
+				dst[dstPos++] = 0b11100000 + ((plain - 4) >> 2);
+				
+				copyBytes(src, srcPos, dst, dstPos, plain);
+				
+				plain = matches[i].location - srcPos;
+			}
+			
+			uint nCopy = matches[i].length;
+			uint offset = matches[i].offset - 1; //subtraction is a part of the transformation for the offset, I think...
+			
+			//apply weird QFS transformations
+			if(nCopy <= 10 && offset < 1024) {
+				if(dstPos + plain + 2 > dst.size())  {
+					return bytes();
+				}
+				
+				// 0oocccpp oooooooo
+				dst[dstPos++] = ((offset >> 3) & 0b01100000) + ((nCopy - 3) << 2) + plain;
+				dst[dstPos++] = offset;
+				
+			} else if(nCopy <= 67 && offset < 16384) {
+				if(dstPos + plain + 3 > dst.size())  {
+					return bytes();
+				}
+				
+				// 10cccccc ppoooooo oooooooo
+				dst[dstPos++] = 0b10000000 + (nCopy - 4);
+				dst[dstPos++] = (plain << 6) + (offset >> 8);
+				dst[dstPos++] = offset;
+				
+			} else if(nCopy <= 1028 && offset < 131072) {
+				if(dstPos + plain + 4 > dst.size())  {
+					return bytes();
+				}
+				
+				// 110occpp oooooooo oooooooo cccccccc
+				dst[dstPos++] = 0b11000000 + ((offset >> 12) & 0b00010000) + (((nCopy - 5) >> 6) & 0b00001100) + plain;
+				dst[dstPos++] = offset >> 8;
+				dst[dstPos++] = offset;
+				dst[dstPos++] = nCopy - 5;
+			}
+			
+			copyBytes(src, srcPos, dst, dstPos, plain);
+			srcPos += nCopy;
+		}
+		
+		//copy the remaining bytes at the end
+		uint plain = src.size() - srcPos;
 		while(plain > 3) {
-			//max possible value is 112
-			//this can only be a multiple of 4, due to the 2-bit right shift in the control character
 			plain = getMin(plain - plain % 4, 112);
 			
-			if(dstPos + plain + 1 > dst.size()) {
+			if(dstPos + plain + 1 > dst.size())  {
 				return bytes();
 			}
 			
@@ -176,170 +234,116 @@ bytes compress(bytes& src) {
 			
 			copyBytes(src, srcPos, dst, dstPos, plain);
 			
-			plain = matches[i].location - srcPos;
+			plain = src.size() - srcPos;
 		}
 		
-		uint nCopy = matches[i].length;
-		uint offset = matches[i].offset - 1; //subtraction is a part of the transformation for the offset, I think...
-		
-		//apply weird QFS transformations
-		if(nCopy <= 10 && offset < 1024) {
-			if(dstPos + plain + 2 > dst.size())  {
+		if(plain > 0) {
+			if(dstPos + plain + 1 > dst.size())  {
 				return bytes();
 			}
 			
-			// 0oocccpp oooooooo
-			dst[dstPos++] = ((offset >> 3) & 0b01100000) + ((nCopy - 3) << 2) + plain;
-			dst[dstPos++] = offset;
-			
-		} else if(nCopy <= 67 && offset < 16384) {
-			if(dstPos + plain + 3 > dst.size())  {
-				return bytes();
-			}
-			
-			// 10cccccc ppoooooo oooooooo
-			dst[dstPos++] = 0b10000000 + (nCopy - 4);
-			dst[dstPos++] = (plain << 6) + (offset >> 8);
-			dst[dstPos++] = offset;
-			
-		} else if(nCopy <= 1028 && offset < 131072) {
-			if(dstPos + plain + 4 > dst.size())  {
-				return bytes();
-			}
-			
-			// 110occpp oooooooo oooooooo cccccccc
-			dst[dstPos++] = 0b11000000 + ((offset >> 12) & 0b00010000) + (((nCopy - 5) >> 6) & 0b00001100) + plain;
-			dst[dstPos++] = offset >> 8;
-			dst[dstPos++] = offset;
-			dst[dstPos++] = nCopy - 5;
-		}
-		
-		copyBytes(src, srcPos, dst, dstPos, plain);
-		srcPos += nCopy;
-	}
-	
-	//copy the remaining bytes at the end
-	uint plain = src.size() - srcPos;
-	while(plain > 3) {
-		plain = getMin(plain - plain % 4, 112);
-		
-		if(dstPos + plain + 1 > dst.size())  {
-			return bytes();
-		}
-		
-		// 111ppppp
-		dst[dstPos++] = 0b11100000 + ((plain - 4) >> 2);
-		
-		copyBytes(src, srcPos, dst, dstPos, plain);
-		
-		plain = src.size() - srcPos;
-	}
-	
-	if(plain > 0) {
-		if(dstPos + plain + 1 > dst.size())  {
-			return bytes();
-		}
-		
-		// 111111pp
-		dst[dstPos++] = 0b11111100 + plain;
-		
-		copyBytes(src, srcPos, dst, dstPos, plain);
-	}
-	
-	//make QFS compression header
-	uint compressedSize = dstPos;
-	uint uncompressedSize = src.size();
-	
-	dst[0] = compressedSize;
-	dst[1] = compressedSize >> 8;
-	dst[2] = compressedSize >> 16;
-	dst[3] = compressedSize >> 24;
-	dst[4] = 0x10;
-	dst[5] = 0xFB;
-	dst[6] = uncompressedSize >> 16;
-	dst[7] = uncompressedSize >> 8;
-	dst[8] = uncompressedSize;
-	
-	return bytes(dst.begin(), dst.begin() + dstPos);
-}
-
-//decompresses src, returns an empty vector if decompression fails
-//it should only fail on broken compressed assets
-bytes decompress(bytes& src) {
-	uint srcPos = 6;
-	uint uncompressedSize = ((uint) src[srcPos++] << 16) + ((uint) src[srcPos++] << 8) + ((uint) src[srcPos++]);
-	
-	bytes dst = bytes(uncompressedSize);
-	uint dstPos = 0;
-	
-	uint b0, b1, b2, b3; //control characters
-	uint nCopy, offset, plain;
-	
-	while(srcPos < src.size()) {
-		b0 = src[srcPos++];
-
-		if(b0 < 0x80) {
-			if(srcPos + 1 > src.size()) {
-				return bytes();
-			}
-			
-			b1 = src[srcPos++];
-			
-			// 0oocccpp oooooooo
-			plain = b0 & 0b00000011; //0-3
-			nCopy = ((b0 & 0b00011100) >> 2) + 3; //3-10
-			offset = ((b0 & 0b01100000) << 3) + b1 + 1; //1-1024
-			
-		} else if(b0 < 0xC0) {
-			if(srcPos + 2 > src.size()) {
-				return bytes();
-			}
-			
-			b1 = src[srcPos++];
-			b2 = src[srcPos++];
-			
-			// 10cccccc ppoooooo oooooooo
-			plain = (b1 & 0b11000000) >> 6; //0-3
-			nCopy = (b0 & 0b00111111) + 4; //4-67
-			offset = ((b1 & 0b00111111) << 8) + b2 + 1; //1-16384
-			
-		} else if(b0 < 0xE0) {
-			if(srcPos + 3 > src.size()) {
-				return bytes();
-			}
-			
-			b1 = src[srcPos++];
-			b2 = src[srcPos++];
-			b3 = src[srcPos++];
-			
-			// 110occpp oooooooo oooooooo cccccccc
-			plain = b0 & 0b00000011; //0-3
-			nCopy = ((b0 & 0b00001100) << 6) + b3 + 5; //5-1028
-			offset = ((b0 & 0b00010000) << 12) + (b1 << 8) + b2 + 1; //1-131072
-			
-		} else if(b0 < 0xFC) {
-			// 111ppppp
-			plain = ((b0 & 0b00011111) << 2) + 4; //4-112
-			nCopy = 0;
-			offset = 0;
-
-		} else {
 			// 111111pp
-			plain = b0 & 0b00000011; //0-3
-			nCopy = 0;
-			offset = 0;
+			dst[dstPos++] = 0b11111100 + plain;
+			
+			copyBytes(src, srcPos, dst, dstPos, plain);
 		}
 		
-		if(srcPos + plain > src.size() || dstPos + plain + nCopy > dst.size())  {
-			return bytes();
-		}
+		//make QFS compression header
+		uint compressedSize = dstPos;
+		uint uncompressedSize = src.size();
 		
-		copyBytes(src, srcPos, dst, dstPos, plain);
+		dst[0] = compressedSize;
+		dst[1] = compressedSize >> 8;
+		dst[2] = compressedSize >> 16;
+		dst[3] = compressedSize >> 24;
+		dst[4] = 0x10;
+		dst[5] = 0xFB;
+		dst[6] = uncompressedSize >> 16;
+		dst[7] = uncompressedSize >> 8;
+		dst[8] = uncompressedSize;
 		
-		//copy bytes from an earlier location in the decompressed output
-		uint fromOffset = dstPos - offset;
-		copyBytes(dst, fromOffset, dst, dstPos, nCopy);
+		return bytes(dst.begin(), dst.begin() + dstPos);
 	}
-	
-	return dst;
+
+	//decompresses src, returns an empty vector if decompression fails
+	//it should only fail on broken compressed assets
+	bytes decompress(bytes& src) {
+		uint srcPos = 6;
+		uint uncompressedSize = ((uint) src[srcPos++] << 16) + ((uint) src[srcPos++] << 8) + ((uint) src[srcPos++]);
+		
+		bytes dst = bytes(uncompressedSize);
+		uint dstPos = 0;
+		
+		uint b0, b1, b2, b3; //control characters
+		uint nCopy, offset, plain;
+		
+		while(srcPos < src.size()) {
+			b0 = src[srcPos++];
+
+			if(b0 < 0x80) {
+				if(srcPos + 1 > src.size()) {
+					return bytes();
+				}
+				
+				b1 = src[srcPos++];
+				
+				// 0oocccpp oooooooo
+				plain = b0 & 0b00000011; //0-3
+				nCopy = ((b0 & 0b00011100) >> 2) + 3; //3-10
+				offset = ((b0 & 0b01100000) << 3) + b1 + 1; //1-1024
+				
+			} else if(b0 < 0xC0) {
+				if(srcPos + 2 > src.size()) {
+					return bytes();
+				}
+				
+				b1 = src[srcPos++];
+				b2 = src[srcPos++];
+				
+				// 10cccccc ppoooooo oooooooo
+				plain = (b1 & 0b11000000) >> 6; //0-3
+				nCopy = (b0 & 0b00111111) + 4; //4-67
+				offset = ((b1 & 0b00111111) << 8) + b2 + 1; //1-16384
+				
+			} else if(b0 < 0xE0) {
+				if(srcPos + 3 > src.size()) {
+					return bytes();
+				}
+				
+				b1 = src[srcPos++];
+				b2 = src[srcPos++];
+				b3 = src[srcPos++];
+				
+				// 110occpp oooooooo oooooooo cccccccc
+				plain = b0 & 0b00000011; //0-3
+				nCopy = ((b0 & 0b00001100) << 6) + b3 + 5; //5-1028
+				offset = ((b0 & 0b00010000) << 12) + (b1 << 8) + b2 + 1; //1-131072
+				
+			} else if(b0 < 0xFC) {
+				// 111ppppp
+				plain = ((b0 & 0b00011111) << 2) + 4; //4-112
+				nCopy = 0;
+				offset = 0;
+
+			} else {
+				// 111111pp
+				plain = b0 & 0b00000011; //0-3
+				nCopy = 0;
+				offset = 0;
+			}
+			
+			if(srcPos + plain > src.size() || dstPos + plain + nCopy > dst.size())  {
+				return bytes();
+			}
+			
+			copyBytes(src, srcPos, dst, dstPos, plain);
+			
+			//copy bytes from an earlier location in the decompressed output
+			uint fromOffset = dstPos - offset;
+			copyBytes(dst, fromOffset, dst, dstPos, nCopy);
+		}
+		
+		return dst;
+	}
+
 }
