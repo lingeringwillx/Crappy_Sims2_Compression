3dcd689d92a3eb3b3b74658d905dc5daee8ecf7b
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2023-11-07 01:34:29 +0300

    Update benchmark

--- benchmark.py
+++ benchmark.py
@@ -4,12 +4,12 @@ import subprocess
 import time
 import sys
 
+#call different executables and test their performance on one file
+
 class Test:
-    def __init__(self, name, args, file_name):
-        self.name = name
-        self.args = args
-        self.args.append("-q")
-        self.file_name = os.path.join(test_directory, file_name) + '.package'
+    def __init__(self, exe, file_name):
+        self.exe = exe
+        self.new_path = os.path.join(test_directory, file_name)
         self.new_size = 0
         self.duration = 0
         
@@ -24,74 +24,50 @@ def get_size_text(size):
     else:
         return '{:.2f} KB'.format(size)
         
+def get_duration_text(duration):
+    if duration > 1:
+        return '{:.2f}s '.format(duration)
+    else:
+        return '{:.2f}ms'.format(duration * 1000)
+        
 test_directory = 'benchmark'
 log_file = 'benchmark.txt'
+    
+exes = sys.argv[1:-1]
+file = sys.argv[-1]
+
+old_size = os.path.getsize(file) / 1024 #KB
 
 if os.path.isfile(log_file):
     os.remove(log_file)
     
-file = sys.argv[1]
-old_size = os.path.getsize(file) / 1024 #KB
-
 if not os.path.isdir('benchmark'):
     os.mkdir('benchmark')
 
-tests = [Test('Decompress', ['-d'], 'decompressed'), Test('Decompress Parallel', ['-d', '-p'], 'decompressedparallel')]
+tests = []
+for i, exe in enumerate(exes):
+    if os.path.isfile(exe) or os.path.isfile(exe + '.exe'):
+        tests.append(Test(exe, file.rsplit('.')[0] + str(i) + '.package'))
+    else:
+        print(exe + ' not found\n')
+        sys.exit()
+        
+print()
 
-for i in range(1, 11, 2):
-    tests.append(Test('Level {}'.format(i), ['-l{}'.format(i)], 'level{}'.format(i)))
-    
-for i in range(1, 11, 2):
-    tests.append(Test('Level {} Parallel'.format(i), ['-l{}'.format(i), '-p'], 'level{}parallel'.format(i)))
-
-for i in range(1, 11, 2):
-    tests.append(Test('Level {} Recompress'.format(i), ['-l{}'.format(i), '-r'], 'level{}recompress'.format(i)))
-    
-for i in range(1, 11, 2):
-    tests.append(Test('Level {} P + R'.format(i), ['-l{}'.format(i), '-r', '-p'], 'level{}parallelrecompress'.format(i)))
-    
 for test in tests:
-    shutil.copy(file, test.file_name)
-    
-    print('{}...'.format(test.name))
+    shutil.copy(file, test.new_path)
     
     t = time.perf_counter()
     
-    subprocess.run(['dbpf-recompress.exe'] + test.args + [test.file_name])
-    test.duration = time.perf_counter() - t
+    subprocess.run([test.exe, test.new_path], stdout=subprocess.DEVNULL)
     
-    test.new_size = os.path.getsize(test.file_name) / 1024
-        
-    os.remove(test.file_name)
+    test.duration = time.perf_counter() - t
+    test.new_size = os.path.getsize(test.new_path) / 1024
+    
+    os.remove(test.new_path)
+    
+    log('{}: {} {:.2f}% {}'.format(test.exe, get_size_text(test.new_size), test.new_size / old_size * 100, get_duration_text(test.duration)))
     
 os.rmdir(test_directory)
 
 print()
-
-log('File Size: {}'.format(get_size_text(old_size)))
-log('Uncompressed Size: {}\n'.format(get_size_text(tests[0].new_size)))
-
-log('{:<21}{:<12}{}\n'.format('Test', 'Size', 'Compression Ratio'))
-
-#decompression + levels non-parallel
-for i, test in enumerate(tests[2:7] + tests[12:17]):
-    log('{:<21}{:<12}{:.2f}%'.format(test.name, get_size_text(test.new_size), test.new_size / old_size * 100))
-
-    if i == 4:
-        log()
-
-log('\n==================================================\n')
-
-#we only care about the speed for the rest of the tests
-log('{:<21}Time\n'.format('Test'))
-
-for i in range(2, 7):
-    for test in tests[i:len(tests):5]:
-        if test.duration > 1:
-            duration_text = '{:.2f}s '.format(test.duration)
-        else:
-            duration_text = '{:.2f}ms'.format(test.duration * 1000)
-            
-        log('{:<21}{}'.format(test.name, duration_text))
-        
-    log()
