668e80681566712951b74b509e38c8895b85ef42
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2024-05-31 11:51:26 +0300

    Compile with /O2

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-cl /EHsc /std:c++17 /openmp dbpf-recompress.cpp
+cl /EHsc /std:c++17 /openmp /O2 dbpf-recompress.cpp
 
 del dbpf-recompress.obj
 
